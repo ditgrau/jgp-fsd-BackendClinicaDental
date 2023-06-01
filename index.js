677a843c74f88a583d64bf0285a0f6d5de20dc62
DitGrau <juditgraup@gmail.com>
2023-06-01 23:20:10 +0200

    router y db declarados, creadas primeras views

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require ('express');
 const app = express();
 const PORT = 5000;
+const db = require ('./db');
+const router = require ('./router');
 
 
 app.use (express.json());
-app.listen (PORT, () => {
-    console.log(`levantado el servidor en el puerto ${PORT}, bienvenida a tu clinica`);
-})
+app.use (router);
+
+db.then ( () => {
+    app.listen (PORT, () => {
+        console.log(`levantado el servidor en el puerto ${PORT}, bienvenida a tu clinica`);
+    })
+}
+)
+.catch (
+    error => console.error (error)
+)
